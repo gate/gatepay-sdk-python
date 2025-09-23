8d9637f9dee6ad282acc9f5e0818907fc6fbdb03
simos-be@gate.me <simos-be@simos-bedeMacBook-Pro.local>
2025-09-23 18:29:18 +0800

    cardNum 别名问题解决

--- src/gatepay/api/model/req/gift/query_req.py
+++ src/gatepay/api/model/req/gift/query_req.py
@@ -1,24 +1,45 @@
 from typing import Optional
-from dataclasses import dataclass
+
+from pydantic import BaseModel, Field
 
 from src.gatepay.base_request import BaseRequest
 from src.gatepay.common.enums.gatepay_api import GatePayApi
 
 
-@dataclass
+
+def to_camel(string: str) -> str:
+    """Convert snake_case to camelCase"""
+    if '_' not in string:
+        return string
+    components = string.split('_')
+    return components[0] + ''.join(x.capitalize() for x in components[1:])
+
+class QueryReqData(BaseModel):
+    card_number: Optional[str] = Field(None, alias='card_number')
+
+    key: Optional[str] = None
+
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+
 class QueryReq(BaseRequest):
     """
     查询礼品卡请求
     """
 
-    card_number: Optional[str] = None
-    key: Optional[str] = None
-
-    def __post_init__(self):
+    def __init__(self):
         """
         初始化后处理，设置API信息
         """
+        super().__init__()
         self.api = GatePayApi.GIFT_QUERY
+
+        # 使用内部数据模型
+        self._data = QueryReqData()
+
+        self.card_number=  None
+        self.key= None
 
     def get_card_number(self) -> Optional[str]:
         """
@@ -35,6 +56,8 @@ class QueryReq(BaseRequest):
         :param card_number: 卡号
         """
         self.card_number = card_number
+        self._data.card_number = card_number
+
 
     def get_key(self) -> Optional[str]:
         """
@@ -51,3 +74,16 @@ class QueryReq(BaseRequest):
         :param key: 密钥
         """
         self.key = key
+        self._data.key = key
+
+    def to_dict(self):
+        """
+        转换为字典，使用驼峰命名以供HTTP传输
+        """
+        # 使用Pydantic的dict方法并启用by_alias选项
+        result = self._data.dict(by_alias=True, exclude_none=True, exclude_defaults=True)
+
+        # 添加父类字段
+        result.update(self.get_dicts())
+
+        return result
