68a5fa29ad1d23e31a3e4d6bfc8801298a540ef5
simos-be@gate.me <simos-be@simos-bedeMacBook-Pro.local>
2025-11-06 22:05:28 +0800

    batch_id 是snake

--- src/gatepay/api/model/req/query_batch_transfer_req.py
+++ src/gatepay/api/model/req/query_batch_transfer_req.py
@@ -1,22 +1,52 @@
 from typing import Optional
-from dataclasses import dataclass
+
+from pydantic import BaseModel, Field
 
 from src.gatepay.base_request import BaseRequest
 from src.gatepay.common.enums.gatepay_api import GatePayApi
 
 
-@dataclass
+def to_camel(string: str) -> str:
+    """Convert snake_case to camelCase"""
+    if '_' not in string:
+        return string
+    components = string.split('_')
+    return components[0] + ''.join(x.capitalize() for x in components[1:])
+
+
+class QueryBatchTransferReqData(BaseModel):
+    """
+    CreateOrderReq的数据模型部分，用于处理Pydantic功能
+    """
+    # 商户系统中的交易号
+    batch_id: Optional[str] = Field(None, alias='batch_id')
+
+    # 订单币种
+    merchant_batch_no: Optional[str] = None
+
+    # 订单金额
+    detail_status: Optional[str] = Field(None, alias='orderAmount')
+
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+
+
+
 class QueryBatchTransferReq(BaseRequest):
 
-    batch_id: Optional[str] = None
-    merchant_batch_no: Optional[str] = None
-    detail_status: Optional[str] = None
-
-    def __post_init__(self):
+    def __init__(self):
         """
         初始化后处理，设置API信息
         """
+        super().__init__()
         self.api = GatePayApi.PAYMENT_QUERY_BATCH_TRANSFER
+
+        self._data = QueryBatchTransferReqData()
+
+        self.batch_id = None
+        self.merchant_batch_no = None
+        self.detail_status = None
 
     def get_batch_id(self) -> Optional[str]:
         """
@@ -33,6 +63,7 @@ class QueryBatchTransferReq(BaseRequest):
         :param batch_id: 批次ID
         """
         self.batch_id = batch_id
+        self._data.batch_id = batch_id
 
     def get_merchant_batch_no(self) -> Optional[str]:
         """
@@ -49,6 +80,7 @@ class QueryBatchTransferReq(BaseRequest):
         :param merchant_batch_no: 商户批次号
         """
         self.merchant_batch_no = merchant_batch_no
+        self._data.merchant_batch_no = merchant_batch_no
 
     def get_detail_status(self) -> Optional[str]:
         """
@@ -65,3 +97,25 @@ class QueryBatchTransferReq(BaseRequest):
         :param detail_status: 详细状态
         """
         self.detail_status = detail_status
+        self._data.detail_status = detail_status
+
+    def to_dict(self):
+        """
+        转换为字典，使用驼峰命名以供HTTP传输
+        """
+        # 使用Pydantic的dict方法并启用by_alias选项
+        result = self._data.dict(by_alias=True, exclude_none=True, exclude_defaults=True)
+
+        # 添加父类字段
+        result.update(self.get_dicts())
+
+        return result
+
+    def __repr__(self) -> str:
+        """
+        返回对象的详细字符串表示
+
+        Returns:
+            str: 对象的详细字符串表示
+        """
+        return self.__str__()
